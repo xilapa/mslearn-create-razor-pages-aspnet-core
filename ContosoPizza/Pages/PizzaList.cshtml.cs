328eab1b9b1bbc82f6a85b1a83e8c47fc478d3bb
xilapa <junior.dirceu@pm.me>
2023-05-24 12:37:51 -0300

    display pizza list

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -1,12 +1,22 @@
-using Microsoft.AspNetCore.Mvc;
+using ContosoPizza.Models;
+using ContosoPizza.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoPiza.Pages
 {
     public class PizzaListModel : PageModel
     {
-        public void OnGet()
+        private readonly PizzaService _service;
+        public IList<Pizza> PizzaList { get; set; } = default!;
+
+        public PizzaListModel(PizzaService service)
         {
+            _service = service;
+        }
+        
+        public void OnGetAsync()
+        {
+            PizzaList = _service.GetPizzas();
         }
     }
 }
