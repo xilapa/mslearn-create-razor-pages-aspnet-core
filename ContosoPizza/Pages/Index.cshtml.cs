cd5abf83962a4781152d929ad04d720143d80923
xilapa <junior.dirceu@pm.me>
2023-05-23 21:17:18 -0300

    first changes

--- ContosoPizza/Pages/Index.cshtml.cs
+++ ContosoPizza/Pages/Index.cshtml.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoPizza.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private static readonly DateTime _openingTime = new (2018, 8, 14);
+        public DateTime OpeningTime = _openingTime;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
